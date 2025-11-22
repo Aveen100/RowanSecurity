d3beb4466744cb185fbe740b4664b099e251ded9
Aveen <aveen.rehman@gmail.com>
2025-11-23 00:41:28 +0500

    Logo added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,7 @@ const Header = () => {
   const [isScrolled, setIsScrolled] = useState(false)
   const [isMenuOpen, setIsMenuOpen] = useState(false)
 
+  
   useEffect(() => {
     const handleScroll = () => {
       setIsScrolled(window.scrollY > 50)
